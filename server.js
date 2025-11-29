9d94da489e049a0433a420a614d22fade064e40a
bigbabylmao2099-ctrl <bigbabylmao2099@gmail.com>
2025-11-30 00:33:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const MODEL_MAPPING = {
   'gpt-4-turbo': 'moonshotai/kimi-k2-instruct-0905',
   'gpt-4o': 'deepseek-ai/deepseek-v3.1',
   'claude-3-opus': 'openai/gpt-oss-120b',
-  'claude-3-sonnet': 'deepseek/deepseek-r1',
+  'claude-3-sonnet': 'deepseek-ai/deepseek-r1-0528',
   'gemini-pro': 'qwen/qwen3-next-80b-a3b-thinking' 
 };
 
